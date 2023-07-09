f9d038a25ae1ef28847f64bb830c4f0c728dde82
Z-Stanislav <125593041+Z-Stanislav@users.noreply.github.com>
2023-07-09 18:00:55 +0300

    Update Leson.java

--- Leson.java
+++ Leson.java
@@ -1,57 +1,20 @@
-import java.io.*;
+import java.util.Map;
 
-public class Z1 {
 
-public static String [] ReadLineFromFile(String pathway) throws Exception {
-BufferedReader br = new BufferedReader(new FileReader(pathway));
-String str;
-int size = 0;
-while ((str = br.readLine()) != null) {
-size +=1;
-}
-br.close();
-String [] listData = new String [size];
+public class Seminar {
+    public static StringBuilder getCondition(Map<String, String> params) {
+        StringBuilder result = new StringBuilder();
 
-int i = 0;
-BufferedReader br1 = new BufferedReader(new FileReader(pathway));
-while ((str = br1.readLine()) != null) {
-listData[i] = str;
-i += 1;
-}
-br1.close();
-return listData;
-
-}
-public static void main(String[] args) throws Exception {
-String [] list = ReadLineFromFile(«data.txt»);
-System.out.println(list[0]);
-StringBuilder resultSelect = LineInList(list[0]);
-System.out.println(resultSelect);
-}
-public static StringBuilder LineInList(String line) {
-String line1 = line.replace(«{«, «»);
-String line2 = line1.replace(«}», «»);
-String line3 = line2.replaceAll(«\»», «»);
-System.out.println(line3);
-StringBuilder result = new StringBuilder(«select * from students where «);
-
-String [] arrayData = line3.split(«, «);
-for (int i =0; i < arrayData.length; i++) {
-String[] arrData = arrayData[i].split(«:»);
-if(arrData[1].equals(«null») == false) {
-if (i != 0) {
-result.append(«, «);
-result.append(arrData[0]);
-result.append(» = «);
-result.append(arrData[1]);
-} else {
-result.append(arrData[0]);
-result.append(» = «);
-result.append(arrData[1]);
-}
-}
-
-}
-return result;
-}
+        for (Map.Entry<String, String> pair: params.entrySet()){
+            try{
+                if (pair.getValue() != null){
+                    if(result.toString().length() > 1) {
+                        result.append(" and ");
+                    }
+                    result.append(pair.getKey()).append(" = '").append(pair.getValue()).append("'");
+                }
+            }catch (NullPointerException ignored){}
+        }
+        return result;
+    }
 }
